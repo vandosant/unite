c36fb320d2bff829dea8b547fe4b92e1bffb0709
vandosant <sjskender@gmail.com>
2018-03-01 20:39:13 -0700

    Add initial graphql setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'
+import AWSAppSyncClient from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
+import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link'
+import { graphql, ApolloProvider, compose } from 'react-apollo'
+import * as AWS from 'aws-sdk'
+import AppSync from './AppSync.js'
 import logo from './logo.svg'
 import './App.css'
+import query from './Queries/schema.graphql'
+
+const client = new AWSAppSyncClient({
+  url: AppSync.graphqlEndpoint,
+  region: AppSync.region,
+  auth: {
+    type: AUTH_TYPE.API_KEY,
+    apiKey: AppSync.apiKey
+
+    // type: AUTH_TYPE.AWS_IAM,
+    // Note - Testing purposes only
+    /*credentials: new AWS.Credentials({
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY
+    })*/
+
+    // Amazon Cognito Federated Identities using AWS Amplify
+    //credentials: () => Auth.currentCredentials(),
+
+    // Amazon Cognito user pools using AWS Amplify
+    // type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    // jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+  }
+})
 
 class App extends Component {
   render () {
