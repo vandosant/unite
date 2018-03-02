cc9c6bd44da245ab1d45d09c52c64aeb8941fccd
vandosant <sjskender@gmail.com>
2018-03-02 10:27:30 -0700

    Remove hardcoded api key

--- src/AppSync.js
+++ src/AppSync.js
@@ -2,5 +2,5 @@ export default {
   'graphqlEndpoint': 'https://alvohcsz2bfs5jnc5ty3nik2am.appsync-api.us-east-1.amazonaws.com/graphql',
   'region': 'us-east-1',
   'authenticationType': 'API_KEY',
-  'apiKey': 'da2-ukv2b3ju6zcbniattjsztokdya'
+  'apiKey': process.env.REACT_APP_API_KEY
 }
